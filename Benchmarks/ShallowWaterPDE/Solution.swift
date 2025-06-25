a2e9d96ecdb532a250ec95d87fc752b695a53346
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-06-25 14:46:13 +0200

    chore: remove no-longer-relevant FIXME comment

--- Benchmarks/ShallowWaterPDE/Solution.swift
+++ Benchmarks/ShallowWaterPDE/Solution.swift
@@ -45,7 +45,6 @@ struct Solution: Differentiable {
 
         for x in 1 ..< resolution - 1 {
             for y in 1 ..< resolution - 1 {
-                // FIXME: Should be u2[x][y] = ...
                 let newValue: Float = 2 * currentWaterLevel[x, y] + (c * c * dt * dt + c * a * dt) * currentWaterLevel.laplace(x, y) * Float(resolution * resolution) - previousWaterLevel[x, y] - c * a * dt * previousWaterLevel.laplace(x, y) * Float(resolution * resolution)
                 newWaterLevel[x, y] = newValue
             }
