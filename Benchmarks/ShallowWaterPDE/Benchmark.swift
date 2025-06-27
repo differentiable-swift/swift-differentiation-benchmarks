bd4301a74ed3c210be8785b04117a1eb57b1a0d6
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-06-27 15:01:03 +0200

    fix: tags and configuration

--- Benchmarks/ShallowWaterPDE/Benchmark.swift
+++ Benchmarks/ShallowWaterPDE/Benchmark.swift
@@ -2,10 +2,21 @@ import Benchmark
 
 let benchmarks: @Sendable () -> Void = {
     let iterationsList = [5, 10, 15, 20, 25]
-    let resolutions = [20, 40, 60, 80, 100]
+    let resolutions = [10, 20, 30, 40, 50]
     for iterations in iterationsList {
         for resolution in resolutions {
-            Benchmark("ShallowWaterPDE", configuration: .init(metrics: [.wallClock, .mallocCountTotal])) { benchmark in
+            Benchmark(
+                "ShallowWaterPDE",
+                configuration: .init(
+                    metrics: [.wallClock, .mallocCountTotal],
+                    tags: [
+                        "iterations": "\(iterations)",
+                        "resolution": "\(resolution)",
+                    ],
+                    maxDuration: .seconds(300),
+                    maxIterations: 20
+                )
+            ) { benchmark in
                 let duration = 10
                 let target = Array2DStorage<Float>.loadTarget(target: Resources.swiftLogo.url, resolution: resolution)
                 
