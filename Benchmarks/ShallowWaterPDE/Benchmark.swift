e7c52e8939437d8cd7df61f39a1292b75227ec73
Chaitanya Kumar <51789253+loonatick-src@users.noreply.github.com>
2025-07-03 15:12:43 +0530

    fix: add resolution=50
    For compatibility with older benchmarks

--- Benchmarks/ShallowWaterPDE/Benchmark.swift
+++ Benchmarks/ShallowWaterPDE/Benchmark.swift
@@ -1,8 +1,8 @@
 import Benchmark
 
 let benchmarks: @Sendable () -> Void = {
-    let durations = [10, 20, 40, 60, 80]
-    let resolutions = [10, 20, 30, 40]
+    let durations = [10, 25, 50, 75]
+    let resolutions = [20, 30, 40, 50]
     for duration in durations {
         for resolution in resolutions {
             Benchmark(
