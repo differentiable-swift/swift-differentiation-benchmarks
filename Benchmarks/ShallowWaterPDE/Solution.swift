74bdce3cb80466d25f9bc9838a1a9288c6b18162
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-07-17 18:45:25 +0200

    refactor: loop interchange for contiguous access

--- Benchmarks/ShallowWaterPDE/Solution.swift
+++ Benchmarks/ShallowWaterPDE/Solution.swift
@@ -43,8 +43,8 @@ struct Solution: Differentiable {
     func evolve() -> Self {
         var newWaterLevel = currentWaterLevel
 
-        for x in 1 ..< resolution - 1 {
-            for y in 1 ..< resolution - 1 {
+        for y in 1 ..< resolution - 1 {
+            for x in 1 ..< resolution - 1 {
                 // FIXME: Should be u2[x][y] = ...
                 let newValue: Float = 2 * currentWaterLevel[x, y] + (c * c * dt * dt + c * a * dt) * currentWaterLevel.laplace(x, y) * Float(resolution * resolution) - previousWaterLevel[x, y] - c * a * dt * previousWaterLevel.laplace(x, y) * Float(resolution * resolution)
                 newWaterLevel[x, y] = newValue
