53910dbc1bdc3e98b8361bb97042c55fc713fa4f
Chaitanya Kumar <chaitanya@passivelogic.com>
2025-07-25 15:54:41 +0200

    chore: remove parameterization of benchmarks

--- Benchmarks/ShallowWaterPDE/Benchmark.swift
+++ Benchmarks/ShallowWaterPDE/Benchmark.swift
@@ -1,36 +1,28 @@
 import Benchmark
 
 let benchmarks: @Sendable () -> Void = {
-    let durations = [10, 25, 50, 75]
-    let resolutions = [20, 40, 50, 60, 80]
-    for duration in durations {
-        for resolution in resolutions {
-            Benchmark(
-                "ShallowWaterPDE",
-                configuration: .init(
-                    metrics: [.wallClock, .mallocCountTotal],
-                    tags: [
-                        "resolution": "\(resolution)",
-                        "duration": "\(duration)",
-                    ],
-                    maxDuration: .seconds(300),
-                    maxIterations: 20
-                )
-            ) { benchmark in
-                let iterations = 10
-                let target = Array2DStorage<Float>.loadTarget(target: Resources.swiftLogo.url, resolution: resolution)
-                
-                benchmark.startMeasurement()
-                let optimizedInitialConditions = Solution.optimization(
-                    target: target,
-                    resolution: resolution,
-                    duration: duration,
-                    iterations: iterations
-                )
-                benchmark.stopMeasurement()
-                
-                blackHole(optimizedInitialConditions)
-            }
-        }
+    Benchmark(
+        "ShallowWaterPDE",
+        configuration: .init(
+            metrics: [.wallClock, .mallocCountTotal],
+            maxDuration: .seconds(300),
+            maxIterations: 20
+        )
+    ) { benchmark in
+        let iterations = 10
+        let duration = 10
+        let resolution = 50
+        let target = Array2DStorage<Float>.loadTarget(
+            target: Resources.swiftLogo.url, resolution: resolution)
+        benchmark.startMeasurement()
+        let optimizedInitialConditions = Solution.optimization(
+            target: target,
+            resolution: resolution,
+            duration: duration,
+            iterations: iterations
+        )
+        benchmark.stopMeasurement()
+
+        blackHole(optimizedInitialConditions)
     }
 }
